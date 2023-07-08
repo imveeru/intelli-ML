e1d9d91ebf2a6114fe53716088d4191821c4246f
imvee <vind.veeru@gmail.com>
2023-07-08 13:20:42 +0530

    Modified page config

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 st.title("Hello World")
 
+#hide streamlit default
 hide_st_style ='''
 <style>
 #MainMenu {visibility: hidden;}
@@ -11,3 +12,10 @@ header {visibility: hidden;}
 </style>
 '''
 st.markdown(hide_st_style, unsafe_allow_html=True)
+
+#page config
+st.set_page_config(
+    page_title="CulinaryCrafter",
+    page_icon="🥣",
+    initial_sidebar_state="expanded",
+)
