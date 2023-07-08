86d94222bd7ce1210fe97a6484d3e32bcdb49ced
imvee <vind.veeru@gmail.com>
2023-07-08 19:25:39 +0530

    Multipage application

--- pages/1_Uploading.py
+++ pages/1_Uploading.py
@@ -20,6 +20,7 @@ import pandas as pd
 if os.path.exists("sourcedata.csv"):
     print("Sourcedata")
     src=pd.read_csv("sourcedata.csv",index_col=None) 
+    st.session_state["source_data"] = src
 
 st.title("Upload your dataset for modelling!")
 file=st.file_uploader("Upload your CSV file here.")
@@ -27,3 +28,4 @@ if file:
     df=pd.read_csv(file,index_col=None)
     df.to_csv("sourcedata.csv",index=None)
     st.dataframe(df)
+    
--- pages/2_Analysis.py
+++ pages/2_Analysis.py
@@ -13,4 +13,12 @@ footer {visibility: hidden;}
 header {visibility: hidden;}
 </style>
 '''
-st.markdown(hide_st_style, unsafe_allow_html=True)  
+st.markdown(hide_st_style, unsafe_allow_html=True) 
+
+import pandas as pd
+from ydata_profiling import ProfileReport
+from streamlit_pandas_profiling import st_profile_report
+
+st.title("Automated Exploratory Data Analysis")
+profile = ProfileReport(st.session_state["source_data"],explorative=True)
+st_profile_report(profile) 
