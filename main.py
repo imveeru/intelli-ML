13004d88e26a9a2b59e9ea48a63bc23bbcecf7c7
imvee <vind.veeru@gmail.com>
2023-07-08 16:27:11 +0530

    ML modelling

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import streamlit as st
 import pandas as pd
 import os
-import pandas_profiling 
+from ydata_profiling import ProfileReport
 from streamlit_pandas_profiling import st_profile_report
+
+from pycaret.regression import setup,compare_models,pull,save_model
 
 #page config
 st.set_page_config(
@@ -27,23 +29,35 @@ with st.sidebar:
     st.info("This application helps you to build an automated ML pipeline.")
 
 if os.path.exists("sourcedata.csv"):
-    df=pd.read_csv("sourcedata.csv",index_col=None)  
+    print("Sourcedata")
+    src=pd.read_csv("sourcedata.csv",index_col=None)  
 
 if choice == "Upload":
     st.title("Upload your dataset for modelling!")
     file=st.file_uploader("Upload your CSV file here.")
     if file:
         df=pd.read_csv(file,index_col=None)
-        df.to_csv("soucedata.csv",index=None)
+        df.to_csv("sourcedata.csv",index=None)
         st.dataframe(df)
-
+ 
 if choice=="Analysis":
     st.title("Automated Exploratory Data Analysis")
-    profile_report=df.profile_report()
-    st_profile_report(profile_report)
+    profile = ProfileReport(src,explorative=True)
+    st_profile_report(profile)
 
 if choice=="Modelling":
-    pass
+    st.title("Modelling")
+    target=st.selectbox("Select the target variable",src.columns)
+    if st.button("Train the model"):
+        setup(src,target=target)
+        setup_df=pull()
+        st.info("Experiment Settings")
+        st.dataframe(setup_df)
+        best_model=compare_models()
+        compare_df=pull()
+        st.info("Model Comparison")
+        st.dataframe(compare_df)
+        save_model(best_model,"best_model")
 
 if choice=="Download":
     pass
