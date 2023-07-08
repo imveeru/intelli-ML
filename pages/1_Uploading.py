de6f4a5bbdd0fbbfe7cdb0d4252e9e32be082848
imvee <vind.veeru@gmail.com>
2023-07-08 19:12:22 +0530

    Multipage application

---
+++ pages/1_Uploading.py
@@ -0,0 +1,16 @@
+import streamlit as st
+st.set_page_config(
+    page_title="IntelliML",
+    page_icon="🤖",
+    initial_sidebar_state="expanded",
+)
+
+#hide streamlit default
+hide_st_style ='''
+<style>
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+header {visibility: hidden;}
+</style>
+'''
+st.markdown(hide_st_style, unsafe_allow_html=True)
