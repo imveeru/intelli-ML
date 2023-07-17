d9ed9be229f35fd1722b09d88fe9f7958a48e828
imvee <vind.veeru@gmail.com>
2023-07-17 23:56:16 +0530

    EDA on own

--- pages/2_Analysis.py
+++ pages/2_Analysis.py
@@ -15,11 +15,54 @@ header {visibility: hidden;}
 '''
 st.markdown(hide_st_style, unsafe_allow_html=True) 
 
+
 import pandas as pd
-from ydata_profiling import ProfileReport
-from streamlit_pandas_profiling import st_profile_report
+import seaborn as sns
+import matplotlib.pyplot as plt
+import seaborn as sns
+import math
+# from ydata_profiling import ProfileReport
+# from streamlit_pandas_profiling import st_profile_report
 
 st.title("Automated Exploratory Data Analysis")
-src=st.session_state["source_data"]
-profile = ProfileReport(src,explorative=True)
-st_profile_report(profile) 
+
+def feature_dist(data):
+    # Set the style of the visualization
+    sns.set(style="whitegrid")
+
+    # Create a figure and a set of subplots
+    rows=math.ceil(len(data.columns)/3)
+    #st.write(rows)
+    fig, axes = plt.subplots(nrows=rows, ncols=3, figsize=(20, 20))
+
+    # Plot histograms
+    for ax, column in zip(axes.flatten(), data.columns):
+        sns.histplot(data[column], kde=True, ax=ax)
+        ax.set_title(column, fontsize=14)
+        ax.tick_params(axis='x', labelrotation=45)
+
+    # Adjust the layout
+    fig.tight_layout()
+    plt.show()
+    st.pyplot(fig)
+
+def
+
+try:
+    src=st.session_state["source_data"]
+    if src.empty:
+        st.error("Upload your dataset before staring analysis!")
+    
+    corr=src.corr()
+    heatmap=sns.heatmap(corr)
+    
+    feature_dist(src)
+    
+except Exception:
+    st.error("Upload your dataset before staring analysis!")
+
+
+
+
+# profile = ProfileReport(src,explorative=True)
+# st_profile_report(profile) 
