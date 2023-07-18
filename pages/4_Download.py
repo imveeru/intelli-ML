2debf330fc9b14c14dd4f0a1e221c35791d3fbdd
imvee <vind.veeru@gmail.com>
2023-07-18 20:53:32 +0530

    Formatting the PDF report

--- pages/4_Download.py
+++ pages/4_Download.py
@@ -18,6 +18,16 @@ st.markdown(hide_st_style, unsafe_allow_html=True)
 st.title("Download the model and report")
 with open("best_model.pkl",'rb') as f:
         st.download_button("Download the best model",f,"best_model.pkl")
-        st.session_state["pdf_report"].output("test.pdf")
+
+st.session_state["pdf_report"].output("final_report.pdf")
+
+with open("final_report.pdf", "rb") as pdf_file:
+    PDFbyte = pdf_file.read()
+
+st.download_button(label="Download Report",
+                    data=PDFbyte,
+                    file_name="Final Report by IntelliML.pdf",
+                    mime='application/octet-stream')
+
 # with open('sourcedata.csv', 'w') as creating_new_csv_file: 
 #    pass 
