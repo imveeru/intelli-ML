5974277d2db9e5a4581f19886ad5e2d2e6331ea8
imvee <vind.veeru@gmail.com>
2023-07-18 18:18:03 +0530

    PDF Report generation

--- pages/1_Uploading.py
+++ pages/1_Uploading.py
@@ -29,8 +29,20 @@ file=st.file_uploader("Upload your CSV file here.")
 if file:
     df=pd.read_csv(file,index_col=None)
     df.to_csv("sourcedata.csv",index=None)
-    st.session_state["pdf_report"]=FPDF(format='A4', unit='in')
+    
+    st.session_state["pdf_report"]=FPDF(format='A4', unit='mm')
+    st.session_state["pdf_report"].set_margin(15)
+    st.session_state["pdf_report"].set_font("Helvetica",size=20,style="B")
+    st.session_state["pdf_report"].set_auto_page_break(auto=True,margin=15)
     st.session_state["pdf_report"].add_page()
+    effective_page_width = st.session_state["pdf_report"].w - 2*st.session_state["pdf_report"].l_margin
+
+    st.session_state["pdf_report"].multi_cell(w=0,txt="# Page 1",ln=True)
+    
+    st.session_state["pdf_report"].set_font("Helvetica",size=12)
+    st.session_state["pdf_report"].multi_cell(w=0,txt="# Page 1",ln=True)
+    st.session_state["pdf_report"].ln(1.5)
+    
     st.subheader("Dataset")
     st.dataframe(df)  
     st.caption("Now click on Analysis button to perform detailed data analysis on the uploaded dataset.")  
