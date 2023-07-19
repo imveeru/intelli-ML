a6ffb400c08b8d0cfc4acb901b48ba9a64330e77
imvee <vind.veeru@gmail.com>
2023-07-20 00:10:25 +0530

    Designing Home page

--- Home.py
+++ Home.py
@@ -23,7 +23,43 @@ header {visibility: hidden;}
 '''
 st.markdown(hide_st_style, unsafe_allow_html=True)
 
-st.title("🤖 IntelliML")
+# st.title("🤖 IntelliML")
+
+st.image("./assets/cover img.jpg")
+
+desc='''
+IntelliML is an innovative and powerful machine learning application developed using Python, leveraging the cutting-edge Google's PALM API. Designed to simplify and accelerate the machine learning process, IntelliML empowers users with the ability to upload their datasets, conveniently select the target variable, and obtain valuable insights while effortlessly trainin.get() multiple machine learning models to find the optimal solution for their data.
+
+**Key Features:**
+
+1. **Effortless Dataset Analysis:** With IntelliML, users can easily upload their datasets in various formats. The app automatically performs an in-depth analysis of the data, identifying patterns, distributions, missing values, and correlations. This preliminary analysis provides users with a comprehensive understanding of their data's characteristics.
+
+2. **Target Variable Selection:** Users can conveniently specify the target variable for their analysis. IntelliML smartly recognizes the dependent variable, which is essential for accurate model training.
+
+3. **Automated Machine Learning:** Leveraging the power of Google's PALM API, IntelliML streamlines the machine learning process by automatically training and evaluating a wide range of machine learning models on the uploaded dataset. This includes popular algorithms like Linear Regression, Decision Trees, Random Forests, Support Vector Machines (SVM), Gradient Boosting, Neural Networks, and more.
+
+4. **Model Comparison:** After the automated training process, IntelliML presents users with a comprehensive comparison of the performance of the various trained models. This comparison helps users make informed decisions about selecting the most appropriate model for their specific dataset.
+
+5. **Best Model Recommendation:** IntelliML intelligently identifies the most suitable machine learning model for the uploaded dataset based on its evaluation metrics, such as accuracy, precision, recall, F1-score, and more. This recommendation ensures that users are equipped with the best possible model to make predictions on their data.
+
+6. **Detailed Report Generation:** Once the analysis and model training are complete, IntelliML generates a detailed and user-friendly report summarizing the entire process. This report includes visualizations, insights, evaluation metrics, and other relevant information to aid in understanding the data and model performance better.
+
+**Why Choose IntelliML:**
+
+- **User-Friendly Interface:** IntelliML is designed with a user-friendly interface, making it accessible to both machine learning novices and experts.
+
+- **Time-Efficient:** By automating the time-consuming tasks of dataset analysis and model training, IntelliML significantly reduces the time needed to build effective machine learning models.
+
+- **Data-Driven Insights:** The app provides valuable insights into the uploaded data, enabling users to make data-driven decisions for their business, research, or projects.
+
+- **State-of-the-Art Model Selection:** IntelliML employs the latest advancements in machine learning technology through Google's PALM API, ensuring the best possible model recommendations.
+
+- **Comprehensive Reports:** Users receive detailed and easy-to-understand reports that can be shared with stakeholders or incorporated into presentations.
+
+Developed by [Veeramanohar](https://github.com/imveeru)
+'''
+
+st.write(desc,markdown=True)
 
 # with st.sidebar:
 #     st.title("🤖 IntelliML")
