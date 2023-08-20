67d1dbb799a732f4a1520e12a2ec8c51ba0226fb
imvee <vind.veeru@gmail.com>
2023-08-20 19:49:38 +0530

    Disabled Export PDF feature

--- pages/1_Upload.py
+++ pages/1_Upload.py
@@ -45,32 +45,32 @@ if file:
     df=pd.read_csv(file,index_col=None)
     df.to_csv("sourcedata.csv",index=None)
     
-    st.session_state["pdf_report"]=FPDF(format='A4', unit='mm')
-    st.session_state["pdf_report"].add_page()
-    # st.session_state["pdf_report"].set_margins(15)
-    st.session_state["pdf_report"].set_auto_page_break(auto=True,margin=15)
-    effective_page_width = st.session_state["pdf_report"].w - 2*st.session_state["pdf_report"].l_margin
-    st.session_state["print_w"]=effective_page_width
+    # st.session_state["pdf_report"]=FPDF(format='A4', unit='mm')
+    # st.session_state["pdf_report"].add_page()
+    # # st.session_state["pdf_report"].set_margins(15)
+    # st.session_state["pdf_report"].set_auto_page_break(auto=True,margin=15)
+    # effective_page_width = st.session_state["pdf_report"].w - 2*st.session_state["pdf_report"].l_margin
+    # st.session_state["print_w"]=effective_page_width
 
     # st.session_state["pdf_report"].set_font("Arial",size=24,style="B")
     # st.session_state["pdf_report"].multi_cell(w=0,txt="IntelliML Report",ln=True,align="C")
     
-    st.session_state["pdf_report"].image("./assets/cover img.jpg",w=st.session_state["print_w"])
+    # st.session_state["pdf_report"].image("./assets/cover img.jpg",w=st.session_state["print_w"])
     
-    st.session_state["pdf_report"].ln(10)
+    # st.session_state["pdf_report"].ln(10)
     
-    st.session_state["pdf_report"].set_font("Arial",size=12,style="B")
-    st.session_state["pdf_report"].set_fill_color(0,150,75)
-    st.session_state["pdf_report"].set_draw_color(0,150,75)
-    st.session_state["pdf_report"].set_line_width(3)
-    st.session_state["pdf_report"].set_text_color(255,255,255)
-    st.session_state["pdf_report"].multi_cell(w=st.session_state["print_w"],h=0,txt="SAMPLE DATASET",align='J',border=True,fill=True)
-    st.session_state["pdf_report"].set_text_color(0,0,0)
-    st.session_state["pdf_report"].set_line_width(0.1)
-    st.session_state["pdf_report"].set_draw_color(30,30,30)
-    st.session_state["pdf_report"].set_fill_color(255,255,255)
-    st.session_state["pdf_report"].ln(2.5)
-    print_df(df.head(10),st.session_state["pdf_report"],effective_page_width)
+    # st.session_state["pdf_report"].set_font("Arial",size=12,style="B")
+    # st.session_state["pdf_report"].set_fill_color(0,150,75)
+    # st.session_state["pdf_report"].set_draw_color(0,150,75)
+    # st.session_state["pdf_report"].set_line_width(3)
+    # st.session_state["pdf_report"].set_text_color(255,255,255)
+    # st.session_state["pdf_report"].multi_cell(w=st.session_state["print_w"],h=0,txt="SAMPLE DATASET",align='J',border=True,fill=True)
+    # st.session_state["pdf_report"].set_text_color(0,0,0)
+    # st.session_state["pdf_report"].set_line_width(0.1)
+    # st.session_state["pdf_report"].set_draw_color(30,30,30)
+    # st.session_state["pdf_report"].set_fill_color(255,255,255)
+    # st.session_state["pdf_report"].ln(2.5)
+    # print_df(df.head(10),st.session_state["pdf_report"],effective_page_width)
     
     st.subheader("Dataset")
     st.dataframe(df)  
